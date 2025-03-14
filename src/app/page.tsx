a10c242fc98a18acca04dab8f3a1827338f81410
caglarbaranbora <boracaglarbaran@gmail.com>
2025-03-14 16:41:40 +0300

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { getUsers, User } from "@/services/api";
 
 export default function Home() {
   const [data, setData] = useState<User[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   useEffect(() => {
     fetchUsers();
@@ -18,13 +19,21 @@ export default function Home() {
     }
   };
 
+  const handleNewUser = () => {
+    if (data.length === 0) return;
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+  };
+
   return (
     <div className="">
       <div className="">merhaba dunya</div>
       <div>
-        {data.map((user) => (
-          <div key={user.id}>{user.username}</div>
-        ))}
+        <button onClick={handleNewUser}>yeni uye getir</button>
+        {data.length > 0 && (
+          <div>
+            <p>{data[currentIndex].username}</p>
+          </div>
+        )}
       </div>
     </div>
   );
